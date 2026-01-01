572c62a10eb6f11d0c6d56569747d05c20cdc33f
sakariyekaamil <sakariyekaamil11@gmail.com>
2026-01-01 11:50:06 +0300

    exercise two of js

---
+++ exercise 2/app.js
@@ -0,0 +1,10 @@
+let a = 15;
+let b = "15"
+console.log(a == b);
+console.log(a != b);
+console.log(a === b);
+console.log(a !== b);
+console.log(a < b);
+console.log(a >b);
+console.log(a <= b);
+console.log(a >= b);
